443daf0788d8e61a71f1d0113281eca5809bd122
ravulaprasad <ravulasaisimhaprasad@gmail.com>
2021-07-30 13:43:16 +0530

    edit

--- rssvprasad_urbanstop/settings.py
+++ rssvprasad_urbanstop/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5u_pm$+jtlzx(7cftha5jb*%!4_n621t8(a)5zlp5d_ajszr_x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
